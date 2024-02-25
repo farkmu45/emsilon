b6a26b3e1db467b859b1bfabc77be0c6b387c4dd
farkmu45 <farukmicros@outlook.com>
2024-02-25 21:12:53 +0800

    Add group detail page

---
+++ resources/views/components/member-item.blade.php
@@ -0,0 +1,7 @@
+<div class="flex cursor-pointer items-center">
+  <div class="relative flex h-10 w-10 items-center justify-center rounded-full"
+    href="#">
+    <img class="h-8 w-8 rounded-full bg-red-700" />
+  </div>
+  <p class="ml-2 text-gray-400">Faruk Maulana</p>
+</div>
---
+++ resources/views/livewire/pages/groups/detail.blade.php
@@ -0,0 +1,40 @@
+<?php
+
+use Livewire\Volt\Component;
+
+new class extends Component {}; ?>
+
+<div>
+  <div class="text-center">
+    <h1 class="text-xl font-semibold">Group 1</h1>
+    <div class="mt-10 flex justify-center">
+      <h3 class="text-2xl font-semibold">341 256 783</h3>
+      <x-heroicon-o-clipboard-document class="ml-1 h-4 text-primary" />
+    </div>
+    <p class="mt-5">This is the code for this group, others can use this code to join this group</p>
+  </div>
+
+  <h3 class="mt-16 text-xl font-bold">Group members</h3>
+  <div class="mt-4">
+    <x-member-item />
+    <x-member-item />
+    <x-member-item />
+  </div>
+
+  <div class="flex flex-col mt-10">
+    <a href="" class="btn btn-primary">Invite friends</a>
+    <button class="btn btn-error text-white mt-3" onclick="disbandModal.showModal()">Disband group</button>
+  </div>
+
+
+  {{-- Disband group modal --}}
+  <x-modal id="disbandModal" title="Are you sure?">
+    All predictions created will be deleted permanently.
+
+    <x-slot:actions>
+        {{-- Notice `onclick` is HTML --}}
+        <x-button label="Cancel" onclick="disbandModal.close()" />
+        <x-button label="Confirm" class="btn-primary" />
+    </x-slot:actions>
+</x-modal>
+</div>
