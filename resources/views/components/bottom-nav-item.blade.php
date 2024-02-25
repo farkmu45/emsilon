2d542644a618e0f97d8e0c4c90ee469ecacac8fe
farkmu45 <farukmicros@outlook.com>
2024-02-25 20:41:58 +0800

    Reduce bottom nav icon size

--- resources/views/components/bottom-nav-item.blade.php
+++ resources/views/components/bottom-nav-item.blade.php
@@ -1,6 +1,6 @@
 @props(['active' => false, 'active-icon', 'icon', 'label', 'link'])
 
 <a class="{{ request()->routeIs($link) ? 'text-primary' : 'text-neutral-400' }}" href="{{ route($link) }}" wire:navigate>
-  @svg($icon, 'w-9 h-9')
+  @svg($icon, 'w-7 h-7')
   <span class="btm-nav-label">{{ $label }}</span>
 </a>
