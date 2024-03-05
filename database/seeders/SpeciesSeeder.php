f3f439a21a06302a3e376d3d4e1e844c0f578df7
farkmu45 <farukmicros@outlook.com>
2024-03-05 20:02:15 +0800

    Add species seeder

---
+++ database/seeders/SpeciesSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use JeroenZwart\CsvSeeder\CsvSeeder;
+
+class SpeciesSeeder extends CsvSeeder
+{
+
+    public function __construct()
+    {
+        $this->file = '/database/species.csv';
+        $this->timestamps = false;
+    }
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::disableQueryLog();
+        parent::run();
+    }
+}
