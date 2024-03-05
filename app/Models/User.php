3ab3273d20266ed9a3fe14ef0b02835db8145041
farkmu45 <farukmicros@outlook.com>
2024-03-05 21:51:13 +0800

    Add personal group creation

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,34 +12,31 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    protected static function booted(): void
+    {
+        static::created(function (User $user) {
+            $group = Group::create(['name' => 'Personal']);
+            Member::create([
+                'group_id' => $group->id,
+                'user_id' => $user->id,
+                'is_creator' => true
+            ]);
+        });
+    }
 }
