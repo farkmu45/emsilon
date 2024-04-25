99a5164851d0d49f7866cd5fdbbfde258602ee39
farkmu45 <farukmicros@outlook.com>
2024-04-25 10:24:45 +0800

    Reduce font and icon size in card browse component

--- resources/views/components/card-browse.blade.php
+++ resources/views/components/card-browse.blade.php
@@ -1,28 +1,28 @@
 <x-card>
   <div class="flex items-center justify-between">
     <div>
-      <p class="mb-4 text-lg font-medium">Oryza Sativa</p>
+      <p class="mb-4 font-medium">Oryza Sativa</p>
       <div class="flex flex-row items-center gap-x-2 text-gray-500">
         <x-heroicon-o-user class="h-5" />
-        <p>Dani</p>
+        <p class="text-sm">Dani</p>
       </div>
       <div class="flex flex-row items-center gap-x-2 text-gray-500">
         <x-heroicon-o-clock class="h-5" />
-        <p>2 minutes ago</p>
+        <p class="text-sm">2 minutes ago</p>
       </div>
     </div>
     <div class="flex flex-col items-end text-end">
       <div>
         <x-dropdown>
           <x-slot:trigger>
-            <x-heroicon-o-ellipsis-horizontal class="w-8 text-gray-500 cursor-pointer" />
+            <x-heroicon-o-ellipsis-horizontal class="w-6 text-gray-500 cursor-pointer" />
           </x-slot:trigger>
           <x-menu-item title="Use as base" />
         </x-dropdown>
       </div>
       <div>
         <p class="text-3xl font-extrabold">95%</p>
-        <p class="text-gray-500">Success rate</p>
+        <p class="text-gray-500 text-sm">Success rate</p>
       </div>
     </div>
   </div>
