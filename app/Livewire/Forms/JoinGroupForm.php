9f07b96dfa5c8311422e880bda7481abc78eb554
farkmu45 <farukmicros@outlook.com>
2024-05-10 20:41:33 +0800

    Add join feature in group menu

---
+++ app/Livewire/Forms/JoinGroupForm.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Livewire\Forms;
+
+use App\Models\Group;
+use Illuminate\Validation\ValidationException;
+use Livewire\Attributes\Validate;
+use Livewire\Form;
+
+class JoinGroupForm extends Form
+{
+    #[Validate('required|string|max:6|min:6|exists:groups,code')]
+    public ?string $code;
+
+    public function join()
+    {
+        $this->validate();
+
+        $group = Group::where('code', $this->code)->first();
+
+        $userGroups = auth()->user()->groups()->toArray();
+
+        $userGroups = array_column($userGroups, 'id');
+
+        if (in_array($group->id, $userGroups)) {
+            $this->addError('code', 'You already joined this group');
+            return false;
+        } else {
+            $result = $group->members()->create([
+                'user_id' => auth()->user()->id,
+                'is_creator' => false
+            ]);
+
+            $this->reset();
+
+            if ($result) {
+                return true;
+            }
+        }
+
+
+        return false;
+    }
+}
--- resources/views/livewire/pages/groups/index.blade.php
+++ resources/views/livewire/pages/groups/index.blade.php
@@ -2,6 +2,7 @@
 
 use Livewire\Volt\Component;
 use App\Livewire\Forms\GroupCreateForm;
+use App\Livewire\Forms\JoinGroupForm;
 use App\Models\Group;
 use Mary\Traits\Toast;
 
@@ -11,7 +12,9 @@ new class extends Component {
     public ?Group $selectedGroup;
     public $groups;
     public GroupCreateForm $createForm;
+    public JoinGroupForm $joinGroupForm;
     public bool $createModal = false;
+    public bool $joinGroupModal = false;
 
     public function mount()
     {
@@ -39,16 +42,33 @@ new class extends Component {
             $this->toast(type: 'error', title: 'An error occured while creating group', position: 'toast-top toast-end', icon: 'o-information-circle');
         }
     }
+
+    public function join()
+    {
+        $result = $this->joinGroupForm->join();
+        if (!$this->getErrorBag()->any()) {
+            $this->joinGroupModal = false;
+
+            if ($result) {
+                $this->groups = auth()->user()->groups();
+                $this->toast(type: 'success', title: 'Successfully joined the group', position: 'toast-top toast-end', icon: 'o-information-circle');
+            } else {
+                $this->toast(type: 'error', title: 'An error occured while joining the group', position: 'toast-top toast-end', icon: 'o-information-circle');
+            }
+        }
+    }
 }; ?>
 
 <div>
   <div class="flex items-center">
     <h1 class="text-3xl font-bold">Your Groups</h1>
-    <a class="link ml-auto font-medium text-primary no-underline"
-      href="{{ $this->selectedGroup ? route('groups.detail', $this->selectedGroup->id) : '/groups/#' }}">Edit group</a>
+    <button class="btn btn-primary fixed bottom-28 right-7 ml-auto lg:static lg:ml-auto"
+      @click="$wire.joinGroupModal = true">
+      Join Group
+    </button>
   </div>
 
-  <div class="mt-5 flex">
+  <div class="mt-5 flex items-center">
     <button class="flex w-20 flex-col items-center" @click="$wire.createModal = true">
       <div class="flex h-10 w-10 items-center justify-center rounded-full bg-primary">
         <x-icon class="h-7 w-7 text-white" name="o-plus" />
@@ -60,8 +80,12 @@ new class extends Component {
     @foreach ($groups as $group)
       <x-group-avatar name="{{ $group->name }}" wire:click="changeActiveGroup({{ $group }})"
         active="{{ $group->id == $selectedGroup->id }}"
-        creator="{{ $this->selectedGroup->members->where('is_creator', true)->first()->user->name }}" wire:key="{{ $group->id }}" />
+        creator="{{ $group->members->where('is_creator', true)->first()->user->name }}"
+        wire:key="{{ $group->id }}" />
     @endforeach
+
+    <a class="link mb-6 ml-auto font-medium text-primary no-underline"
+      href="{{ $this->selectedGroup ? route('groups.detail', $this->selectedGroup->id) : '/groups/#' }}">Edit group</a>
   </div>
 
   <div class="mt-10 grid gap-4 lg:grid-cols-3">
@@ -81,6 +105,19 @@ new class extends Component {
     <x-slot:actions>
       <x-button @click="$wire.createModal = false" label="Cancel" onclick="createModal.close()" />
       <x-button class="btn-primary" wire:click="create" label="Submit" />
+    </x-slot:actions>
+  </x-modal>
+
+
+  {{-- Create group modal --}}
+  <x-modal class="backdrop-blur" title="Join group via code" wire:model="joinGroupModal">
+    <form method="post" wire:submit="create">
+      <x-custom-input label="Enter code" wire:model="joinGroupForm.code" autofocus required />
+    </form>
+
+    <x-slot:actions>
+      <x-button @click="$wire.joinGroupModal = false" label="Cancel" onclick="joinGroupModal.close()" />
+      <x-button class="btn-primary" wire:click="join" label="Join group" />
     </x-slot:actions>
   </x-modal>
 </div>
