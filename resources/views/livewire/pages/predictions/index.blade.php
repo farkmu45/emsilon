89cd0e12787703f8406f4d24f232f11bd1e4d979
farkmu45 <farukmicros@outlook.com>
2024-05-11 02:29:11 +0800

    Add infinite loading in prediction history

--- resources/views/livewire/pages/predictions/index.blade.php
+++ resources/views/livewire/pages/predictions/index.blade.php
@@ -4,13 +4,23 @@ use Livewire\Volt\Component;
 use App\Models\Prediction;
 
 new class extends Component {
-    public $predictions;
+    public $perPage = 10;
 
-    public function mount()
+    public function with(): array
     {
-        $this->predictions = Prediction::where('user_id', auth()->user()->id)
-            ->whereHas('group', fn($query) => $query->where('name', 'Personal'))
-            ->get();
+        $personalGroup = auth()->user()->personalGroup();
+        return [
+            'predictions' => Prediction::where('user_id', auth()->user()->id)
+                ->whereHas('group', fn($query) => $query->where('name', 'Personal'))
+                ->latest()
+                ->take($this->perPage)
+                ->get(),
+        ];
+    }
+
+    public function loadMore()
+    {
+        $this->perPage += 10;
     }
 }; ?>
 
@@ -26,6 +36,10 @@ new class extends Component {
   </div>
 
   @if (!count($predictions))
-    <x-data-empty class="mt-24" label="No predictions available"/>
+    <x-data-empty class="mt-24" label="No predictions available" />
   @endif
+
+  <div class="flex w-full items-center justify-center p-12" x-intersect.full="$wire.loadMore()">
+    <x-loading class="loading-lg text-primary" wire:loading wire:target="loadMore" />
+  </div>
 </div>
