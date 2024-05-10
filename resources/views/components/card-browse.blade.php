84c69c9046a5771c4a1f26490948185bdd4cc3cd
farkmu45 <farukmicros@outlook.com>
2024-05-10 19:15:43 +0800

    Add browse page feature

--- resources/views/components/card-browse.blade.php
+++ resources/views/components/card-browse.blade.php
@@ -1,28 +1,30 @@
+@props(['result', 'creator', 'species', 'suitabilityRate', 'creator', 'createdAt', 'link'])
+
 <x-card>
   <div class="flex items-center justify-between">
     <div>
-      <p class="mb-4 font-medium">Oryza Sativa</p>
+      <p class="mb-4 font-medium">{{ \Illuminate\Support\Str::limit($species, 20) }}</p>
       <div class="flex flex-row items-center gap-x-2 text-gray-500">
         <x-heroicon-o-user class="h-5" />
-        <p class="text-sm">Dani</p>
+        <p class="text-sm">{{ $creator }}</p>
       </div>
-      <div class="flex flex-row items-center gap-x-2 text-gray-500">
+      <div class="mt-1 flex flex-row items-center gap-x-2 text-gray-500">
         <x-heroicon-o-clock class="h-5" />
-        <p class="text-sm">2 minutes ago</p>
+        <p class="text-sm">{{ \Carbon\Carbon::parse($createdAt)->diffForHumans() }}</p>
       </div>
     </div>
     <div class="flex flex-col items-end text-end">
       <div>
         <x-dropdown>
           <x-slot:trigger>
-            <x-heroicon-o-ellipsis-horizontal class="w-6 text-gray-500 cursor-pointer" />
+            <x-heroicon-o-ellipsis-horizontal class="w-6 cursor-pointer text-gray-500" />
           </x-slot:trigger>
-          <x-menu-item title="Use as base" />
+          <x-menu-item title="Use as base" link="{{ $link }}" />
         </x-dropdown>
       </div>
       <div>
-        <p class="text-3xl font-extrabold">95%</p>
-        <p class="text-gray-500 text-sm">Suitability rate</p>
+        <p class="text-3xl font-extrabold">{{ $suitabilityRate }}%</p>
+        <p class="text-sm text-gray-500">Suitability rate</p>
       </div>
     </div>
   </div>
--- resources/views/livewire/browse.blade.php
+++ resources/views/livewire/browse.blade.php
@@ -1,19 +1,33 @@
 <?php
 
 use Livewire\Volt\Component;
+use App\Models\Prediction;
 
 new class extends Component {
-    //
+    public $perPage = 10;
+    public $predictions;
+
+    public function mount()
+    {
+        $personalGroup = auth()->user()->personalGroup();
+        $this->predictions = Prediction::whereHas('group', fn($query) => $query->where('name', 'Personal')->whereNot('id', $personalGroup->id))
+            ->where('result', 1)
+            ->latest()
+            ->paginate($this->perPage)
+            ->items();
+    }
 }; ?>
 
 <div>
   <h1 class="text-3xl font-bold">Browse</h1>
   <h3 class="mt-2 text-lg font-medium text-neutral-600">Find any mutagenesis treatment to use as prediction base</h3>
 
-  <div class="grid lg:grid-cols-3 lg:gap-x-3 gap-y-4 mt-10">
-    <x-card-browse />
-    <x-card-browse />
-    <x-card-browse />
-    <x-card-browse />
+  <div class="mt-10 grid gap-y-4 lg:grid-cols-3 lg:gap-x-3">
+    @foreach ($predictions as $prediction)
+      <x-card-browse result="{{ $prediction->result }}" species="{{ $prediction->species->name }}"
+        suitabilityRate="{{ $prediction->success_rate }}" success="{{ $prediction->result }}"
+        createdAt="{{ $prediction->created_at }}" creator="{{ $prediction->user->name }}"
+        link="{{ route('predictions.create', ['predictionId' => $prediction->id]) }}" />
+    @endforeach
   </div>
 </div>
