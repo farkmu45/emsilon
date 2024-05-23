14cddcddd6b1167ee78d1b2920a740624ef9823e
farkmu45 <farukmicros@outlook.com>
2024-05-23 08:38:25 +0800

    Update css asset

--- resources/views/components/layouts/guest.blade.php
+++ resources/views/components/layouts/guest.blade.php
@@ -17,7 +17,7 @@
 </head>
 
 <body class="font-sans text-gray-900 antialiased">
-  <div class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
+  <div class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center px-4">
 
     <div class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
       {{ $slot }}
